0ca4a537ddb7141e6513fb4299e2e48b6a44623b
danielcranky <danielcranky@gmail.com>
2020-05-19 17:41:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ if (isset($_REQUEST['stones']) && ($_REQUEST['beatles'])) {
     $x = $_REQUEST['stones'];
     $y = $_REQUEST['beatles'];
     list ($left_space, $right_space) = findSpace ($x, $y);
-    //echo "{$left_space} . {$right_space}";
 }
 
 include "view.php";
