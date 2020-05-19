b9460fcacb8ccbffa991b2a47c4431a851eac749
danielcranky <danielcranky@gmail.com>
2020-05-19 17:24:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Test</title>
-    <meta charset="utf-8">
-</head>
-<body>
-<form action="findSpace.php" method="POST">
-    Количество камней: <input type="number" name="stones" /><br><br>
-    Количество жуков: <input type="number" name="beatles" /><br><br>
-    <input type="submit" value="Посчитать">
-</form>
-
 <?php
+require_once "findSpace.php";
+
 if (isset($_REQUEST['stones']) && ($_REQUEST['beatles'])) {
     $x = $_REQUEST['stones'];
     $y = $_REQUEST['beatles'];
     list ($left_space, $right_space) = findSpace ($x, $y);
-    echo $left_space, ',', $right_space;
+    //echo "{$left_space} . {$right_space}";
 }
-?>
 
+include "view.php";
